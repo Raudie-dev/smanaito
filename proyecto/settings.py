d9e2ddb572610d4a21bd2ffead443ff53215f209
Juan <juandiegoaranaperez@gmail.com>
2026-08-10 18:58:48 -0400

    Configurar soporte de base de datos Postgres (dj-database-url, psycopg2), uvicorn worker para gunicorn y orígenes CSRF para Render

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://demos.raudie.net',
+    'https://*.onrender.com',
 ]
 
 # Application definition
@@ -78,11 +79,13 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 
